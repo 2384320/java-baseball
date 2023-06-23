2ea4b6d9e0ebb35d6e77ead98b23cde14df91ebb
2384320 <chdms1549@gmail.com>
2023-06-23 17:25:08 +0900

    feat: 게임 전 세팅 기능 추가
    - 랜덤 숫자 3개 저장 기능 구현
    - 숫자 중 중복 검사 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,50 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
+    private final static int gameStart = 1;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> computerNumber = new ArrayList<>();
+
+        do {
+            setGame(computerNumber);
+
+
+
+
+        } while (false);
+
+    }
+
+    private static String getOutputString(int command) {
+        String outputString = "";
+        if (command == gameStart) outputString = "숫자 야구 게임을 시작합니다.";
+
+        return outputString;
+    }
+
+    private static void printOutputString(int command) {
+        System.out.println(getOutputString(command));
+    }
+
+    private static boolean isDuplicate(List<Integer> computerNumber, int randomNumber) {
+        return computerNumber.contains(randomNumber);
+    }
+
+    private static int getRandomNumber() {
+        return Randoms.pickNumberInRange(1, 9);
+    }
+
+    public static void setGame(List<Integer> computerNumber) {
+        printOutputString(gameStart);
+
+        while (computerNumber.size() < 3) {
+            int randomNumber = getRandomNumber();
+            if (isDuplicate(computerNumber, randomNumber)) continue;
+            computerNumber.add(randomNumber);
+        }
     }
 }
